81bd4cab7a59d400f716ccda49e7383807216a27
Lumin228 <6786679@gmail.com>
2023-12-15 01:03:48 +0200

    End

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,107 @@
-import { foo } from "./links";
+import { foo } from './links';
+import Notiflix from 'notiflix';
 
 const refs = {
-    form: document.querySelector('form')
+  form: document.querySelector('form'),
+  imageDiv: document.querySelector('.gallery'),
+  buttonLoad: document.querySelector('button[type="button"]'),
 };
 
+const TXT_CACHE_LOCAL = "TXT_CAHE";
+
+let i = 1;
+
+
 refs.form.addEventListener('submit', inputFunc);
+refs.buttonLoad.addEventListener('click', loadMoreFunc);
 
-function inputFunc(event) {
-    event.preventDefault(); 
-    const formElements = event.currentTarget.elements;
-    const inputValue = formElements['searchQuery'].value;
-    console.log(inputValue);
+async function inputFunc(event) {
+  event.preventDefault();
+  
+  const formElements = event.currentTarget.elements;
+  const inputValue = formElements['searchQuery'].value;
+  localStorage.setItem(TXT_CACHE_LOCAL, JSON.stringify(inputValue))
+  refs.imageDiv.innerHTML = '';
+  i = 1;
+  try {
+    
+    const data = await foo(inputValue, i);
+    refs.imageDiv.classList.remove('is-hidden');
+    
+    data.map(key => {
+      const createImgDiv = `
+        <div class="photo-card">
+          <img src="${key.previewURL}" alt="${key.user}" loading="lazy" width="285" height="250" class="image" >
+          <div class="info">
+            <p class="info-item">
+              <b>Likes</b>
+              <b>${key.likes}</b>
+            </p>
+            <p class="info-item">
+              <b>Views</b>
+              <b>${key.views}</b>
+            </p>
+            <p class="info-item">
+              <b>Comments</b>
+              <b>${key.comments}</b>
+            </p>
+            <p class="info-item">
+              <b>Downloads</b>
+              <b>${key.downloads}</b>
+            </p>
+          </div>
+        </div>`;
+
+      refs.imageDiv.innerHTML += createImgDiv;
+      
+    });
+    Notiflix.Notify.success(`You've found ${localStorage.getItem(TXT_CACHE_LOCAL)}`);
+  } catch (error) {
+    Notiflix.Notify.failure(`Sorry, but there is no such images as ${localStorage.getItem(TXT_CACHE_LOCAL)}`);
+  }
 }
 
-async function loadPage(v) {
 
+
+async function loadMoreFunc() {
+    i ++;
+    try {
+
+        const data = await foo(localStorage.getItem(TXT_CACHE_LOCAL), i);
+        Notiflix.Notify.success(`You've loaded ${localStorage.getItem(TXT_CACHE_LOCAL)} in count of ${data.length}`);
+        if (data.length == 0) {
+          console.log("no more images")
+        }
+    
+        data.map(key => {
+          const createImgDiv = `
+            <div class="photo-card">
+              <img src="${key.previewURL}" alt="${key.user}" loading="lazy" width="285" height="250" class="image" >
+              <div class="info">
+                <p class="info-item">
+                  <b>Likes</b>
+                  <b>${key.likes}</b>
+                </p>
+                <p class="info-item">
+                  <b>Views</b>
+                  <b>${key.views}</b>
+                </p>
+                <p class="info-item">
+                  <b>Comments</b>
+                  <b>${key.comments}</b>
+                </p>
+                <p class="info-item">
+                  <b>Downloads</b>
+                  <b>${key.downloads}</b>
+                </p>
+              </div>
+            </div>`;
+    
+          refs.imageDiv.innerHTML += createImgDiv;
+          console.log(i)
+        });
+      } catch (error) {
+        console.error(error);
+      }
 }
 
-const q = "yellow+flowers";
-
-foo()
--- src/js/links.js
+++ src/js/links.js
@@ -1,17 +1,23 @@
 import axios from "axios";
 
+const orientetion = 'horizontal';
+const safesearch = 'true';
 
+export async function foo(que, page) {
 
-export async function foo() {
-
-
-  const value = await axios.get(`https://pixabay.com/api/?key=41243216-d0c3aacbdbd6d0005d29ac5a4&q=yellow+flowers&image_type=photo&per_page=40`)
+  
+  const value = await axios.get(`https://pixabay.com/api/?key=41243216-d0c3aacbdbd6d0005d29ac5a4&q=${que}&image_type=photo&page=${page}&per_page=40&orientation=${orientetion}&safesearch=${safesearch}`)
   .then(response => {
-    console.log(response.data);
+    if(response.data.hits.length >= 20) {
+  return response.data.hits;
+}
+   else{
+    throw Error
+   }
   })
   .catch(error => {
     console.error(error);
   });
-
   return value
 }
+
